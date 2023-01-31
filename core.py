940f57e603f387e517a8cab86d41c4dffee9e651
Kasra <kasra138079@gmail.com>
2023-01-31 20:37:36 +0330

    fixes

--- core.py
+++ core.py
@@ -1,3 +1,52 @@
-import math
+from numpy import random
 
-print("hello there")
+class Process:
+    def __init__(self, dropped=False, service_time=0, priority='Low',timeout=0, arrival=0,processed=0) -> None:
+        self.dropped = dropped
+        self.service_time = service_time
+        self.priority = priority
+        self.timeout = timeout
+        self.time_in_queue = 0
+        self.arrival = arrival
+        self.processed = processed
+        self.previous_queue = 'High'
+
+
+class Processor:
+    def __init__(self, t1, t2) -> None:
+        self.clock = 0
+        self.is_busy = False
+        self.is_empty = False
+        self.RR1 = []
+        self.RR2 = []
+        self.FCFS = []
+        self.t1 = t1
+        self.t2 = t2
+        self.processes_dropped = 0
+        self.init_queue = []
+        self.current_process = None
+
+    def dispatcher(self, queue='Low'):
+        if queue == 'Low':
+            p = self.FCFS.pop(0)
+        elif queue == 'Normal':
+            p = self.RR2.pop(0)
+        else:
+            p = self.RR1.pop(0)
+        
+        self.current_process = p
+        self.is_busy = True
+
+    def job_creator():
+        pass
+
+    def job_loader(self, k):
+        counter=0
+        for p in self.init_queue:
+            if p.arrival <= self.clock:
+                self.RR1.append(p)
+                counter += 1
+            if counter == k:
+                break
+        
+
--- main.py
+++ main.py
@@ -1,8 +1,58 @@
 from numpy import random
 import math
+from core import Process, Processor
 
 x = input('X: ')
 y = input('Y: ')
 z = input('Z: ')
 sim_time = input('Time: ')
 process_count = input('Desired process count: ')
+k = input('Provide Process count minimum: ')
+t1 = input('T1: ')
+t2 = input('T2: ')
+timer = 0
+
+aux_list = []
+for i in range(process_count):
+    exp1 = random.exponential(x)
+    exp2 = random.exponential(y)
+    exp3 = random.exponential(z)
+    uni = random.uniform()
+    exp1 = math.ceil(exp1)
+    exp2 = math.ceil(exp2)
+    exp3 = math.ceil(exp3)
+    priority = "High"
+    if uni<0.7:
+        priority = 'Low'
+    elif uni<0.9:
+        priority = 'Normal'
+
+    process = Process(False,exp2,priority,exp3, exp1+timer)
+    timer += exp1
+    aux_list.append(process)
+
+high = []
+mid = []
+low = []
+
+for p in aux_list:
+    if p.priority == 'High':
+        high.append(p)
+    elif p.priority == 'Normal':
+        mid.append(p)
+    else:
+        low.append(p)
+
+high.sort(key=lambda x: x.service_time, reverse=True)
+mid.sort(key=lambda x: x.service_time, reverse=True)
+low.sort(key=lambda x: x.service_time, reverse=True)
+
+processor = Processor(t1, t2)
+processor.init_queue.extend(high)
+processor.init_queue.extend(mid)
+processor.init_queue.extend(low)
+
+while (not processor.is_empty) and (processor.clock<sim_time):
+    if len(processor.FCFS)+len(processor.RR1)+len(processor.RR2):
+        processor.job_loader()
+    
